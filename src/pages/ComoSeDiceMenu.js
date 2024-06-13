76af8a8e9e89614cfd96d1ef81ba106735c620d9
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-06-12 23:10:17 -0400

    Added the normal mode card to the main menu page.

--- src/pages/ComoSeDiceMenu.js
+++ src/pages/ComoSeDiceMenu.js
@@ -1,10 +1,28 @@
 import '../css/App.css';
 import HomeNavigationBar from '../components/NavigationBar.jsx'
+import GameModeCard from '../components/CardWithGameModes.jsx'
 
 function Home() {
   return (
-    <div className="normalGameMode">
+    <div>
       <HomeNavigationBar></HomeNavigationBar>
+
+      <div className='flex justify-center'>
+        <div className='w-5/6'>
+          <div className='grid grid-rows-2 grid-flow-col gap-4'>
+            <div>
+              <GameModeCard
+                title="Start Singleplayer" 
+                body="Play Cómo Se Dice Alone">
+              </GameModeCard>
+            </div>
+            <div className='h-20'>02</div>
+            <div className='h-20'>03</div>
+            <div className='h-20'>04</div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
