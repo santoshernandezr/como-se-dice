9fcac380c0f0925293d859caf4dedd07a63f0b2b
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-06-27 21:41:09 -0400

    Adding the coming soon to the leaderboard button.

--- client/src/components/common/NavigationBar.jsx
+++ client/src/components/common/NavigationBar.jsx
@@ -55,7 +55,7 @@ function NavigationBar() {
                             <a href="/comosedice" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</a>
                         </li>
                         <li>
-                            <a href="/placeholderUntilLeaderBoardPageIsDone" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leaderboard</a>
+                            <a href="/comingSoon" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leaderboard</a>
                         </li>
                         </ul>
                     </div>
