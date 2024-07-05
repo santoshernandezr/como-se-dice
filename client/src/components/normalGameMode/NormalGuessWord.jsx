beefeb78985394ab39a05e2b35db7e3bee08f5d3
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-07-05 10:08:42 -0500

    Initial refactor changes to NormalGuessWord.jsx

--- client/src/components/normalGameMode/NormalGuessWord.jsx
+++ client/src/components/normalGameMode/NormalGuessWord.jsx
@@ -3,29 +3,6 @@ import PlayerInfo from './PlayerInfo';
 import confetti from "canvas-confetti"
 
 /**
- * Determines the message that the will go in the 'decidingContainer' which the user will see, either that they
- * won or they lost. Hides the 'wordContainer', 'guessField', and 'guessButton', and shows the 'decidingContainer,
- * and 'playButton'. 
- * 
- * @param { innerHTML } container in which the win or lose message will be set.
- * @param { innerHTML } container in which the word is in.
- * @param { InputHTMLAttributes } guessField HTML input attribute.
- * @param { ButtonHTMLAttributes } guessButton HTML button attribute.
- * @param { String } message that will be posted in the container.
- * @param { ButtonHTMLAttributes } playAgainButton HTML button attribute.
- */
-function determineOutcome(decidingContainer, wordContainer, guessField, guessButton, message, playAgainButton) {
-    decidingContainer.innerHTML = message;
-    // Hide the following elements.
-    wordContainer.style.display = 'none'
-    guessField.style.display = 'none';
-    guessButton.style.display = 'none';
-    // Show the following elements.
-    decidingContainer.style.display = 'inline'
-    playAgainButton.style.display = "inline";
-}
-
-/**
  * Validate strings are alphanumeric.
  * 
  * @param { String } string which is being validated.
@@ -36,28 +13,6 @@ function isAlphanumeric(str) {
 }
 
 /**
- * Resets the game after the 'play again' button is pressed. This will show the 'decidingContainer' and 'playAgainButton' and hide the 'guessButton', 'guessField',
- * 'nextWordContainer', and clear the 'guessField'.
- */
-function playAgainReset() {
-    let guessButton = document.getElementById('guessButton');
-    let playAgainButton = document.getElementById('playAgainButton');
-    let guessField = document.getElementById('guessField');
-    let nextWordContainer = document.getElementById('nextWordContainer');
-    let decidingContainer = document.getElementById('decidingContainer');
-
-    // Hide the following elements.
-    decidingContainer.style.display = 'none'
-    playAgainButton.style.display = 'none';
-    // Show the following elements.
-    guessButton.style.display = 'inline'
-    guessField.style.display = 'inline';
-    nextWordContainer.style.display = 'inline'
-    guessField.value = ''
-}
-
-
-/**
  * Component that handles the creation of the text field that will ask the user what word is being guessed,
  * the input field for the user to guess, and the button that triggers the validation of the users guess.
  * 
@@ -66,14 +21,24 @@ function playAgainReset() {
 function GuessWord() {
     // Instantiate the words state and set it to "loading" until it gets updated.
     const [words, setWords] = useState("loading")
+    // Instantiate the current word state and set it to the first word 
+    const [currentWord, setCurrentWord] = useState(words[0])
+    // Instantiate the user guess state.
+    const [userGuess, setUserGuess] = useState("")
     // Instantiate the score state and set it to 0.
     const [score, setScore] = useState(0)
     // Instantiate the lives state and set it to 3.
     const [lives, setLives] = useState(3)
     // Instantiate the index state and set it to 0.
     const [index, setIndex] = useState(0)
-    // Instantiate the current word state and set it to the first word 
-    const [currentWord, setCurrentWord] = useState(words[0])
+
+    // Components that will show and not show, depending if the user wins or loses.
+    const [nextWordContainer, setNextWordContainer] = useState(true);
+    const [guessField, setGuessField] = useState(true);
+    const [guessButton, setGuessButton] = useState(true);
+
+    const [decidingContainer, setDecidingContainer] = useState(false);
+    const [playAgainButton, setPlayAgainButton] = useState(false);
 
     // Functions that will increment or decrement a state using the previous state.
     function incrementScore() {
@@ -104,7 +69,13 @@ function GuessWord() {
     */
     async function playAgain() {
         fetchWords();
-        playAgainReset();
+        setNextWordContainer(true);
+        setGuessField(true);
+        setGuessButton(true);
+        
+        setDecidingContainer(false);
+        setPlayAgainButton(false);
+        document.getElementById('guessField').value = '';
 
         setIndex(0);
         setLives(3);
@@ -133,12 +104,13 @@ function GuessWord() {
     useEffect(() => {
 
     if (score == 10) {
-        let nextWordContainer = document.getElementById('nextWordContainer');
-        let decidingContainer = document.getElementById('decidingContainer');
-        let guessField = document.getElementById('guessField');
-        let guessButton = document.getElementById("guessButton");
-        let playAgainButton = document.getElementById("playAgainButton");
-        determineOutcome(decidingContainer, nextWordContainer, guessField, guessButton, "You Win!", playAgainButton);
+        setNextWordContainer(false);
+        setGuessField(false);
+        setGuessButton(false);
+        
+        setDecidingContainer(true);
+        setPlayAgainButton(true);
+        document.getElementById('decidingContainer').innerHTML = "You Win!";
     }
 
     }, [score])
@@ -150,12 +122,13 @@ function GuessWord() {
     useEffect(() => {
 
     if (lives == 0) {
-        let nextWordContainer = document.getElementById('nextWordContainer');
-        let decidingContainer = document.getElementById('decidingContainer');
-        let guessField = document.getElementById('guessField');
-        let guessButton = document.getElementById("guessButton");
-        let playAgainButton = document.getElementById("playAgainButton");
-        determineOutcome(decidingContainer, nextWordContainer, guessField, guessButton, "You ran out of lives, try again.", playAgainButton);
+        setNextWordContainer(false);
+        setGuessField(false);
+        setGuessButton(false);
+        
+        setDecidingContainer(true);
+        setPlayAgainButton(true);
+        document.getElementById('decidingContainer').innerHTML = "You ran out of lives, try again.";
     }
 
     }, [lives])
@@ -167,13 +140,10 @@ function GuessWord() {
         // This prevents the eventHandler from refershing the page. We don't want the page to refresh until the game is finished.
         e.preventDefault();
 
-        let guessField = document.getElementById('guessField');
-        const userGuess = guessField.value.toLowerCase().trim();
-
         // Check that the users guess is Alphanumeric
         if(isAlphanumeric(userGuess)) {
             // Check if the users guess is correct. If so, increment the score and the index.
-            if (userGuess === currentWord.english.toLowerCase()) {
+            if (userGuess.toLowerCase() === currentWord.english.toLowerCase()) {
                 confetti();
                 incrementScore();
                 incrementIndex();
@@ -199,10 +169,10 @@ function GuessWord() {
             The second container, 'decidingContainer', contains the win or lose message the user will see at the end of the game.
         */}
         <div className="flex justify-center" >
-            <h2 id="nextWordContainer" className="w-1/4 flex justify-center text-center" style={{ display: 'inline' }}>
-                ¿Cómo Se Dice&nbsp;<p id="currentWord" className="font-bold" style={{ display: 'inline'}}>{currentWord.spanish}({currentWord.type})?</p>
+            <h2 id="nextWordContainer" className="w-1/4 flex justify-center text-center" style={{ display: nextWordContainer ? 'inline': 'none' }}>
+                ¿Cómo Se Dice&nbsp;<p className="font-bold" >{currentWord.spanish}({currentWord.type})?</p>
             </h2>
-            <h2 id="decidingContainer" className="w-1/4 flex justify-center text-center" style={{ display: 'none' }}>
+            <h2 id="decidingContainer" className="w-1/4 flex justify-center text-center" style={{ display: decidingContainer ? 'inline': 'none' }}>
             </h2>
         </div>
 
@@ -213,7 +183,7 @@ function GuessWord() {
         <form onSubmit={determineInput}>
             {/* Input field for the users guess. */}
             <div className="flex justify-center">
-                <input autoComplete="one-time-code" type="text" id="guessField" className="block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-96 "></input>
+                <input style={{ display: guessField ? 'inline': 'none' }} onChange={(e) => setUserGuess(e.target.value)} autoComplete="one-time-code" type="text" id="guessField" className="block p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-96 "></input>
             </div>
             
             
@@ -223,9 +193,9 @@ function GuessWord() {
                 Second button is the playAgainButton which is the button that the user will press if they want to play again, which will be shown at the end of the game.
             */}
             <div className="flex justify-center">
-                <button style={{ display: 'inline' }} onClick={determineInput} id="guessButton" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-20 mt-4">Guess</button>
+                <button style={{ display: guessButton ?'inline': 'none' }} onClick={determineInput} id="guessButton" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-20 mt-4">Guess</button>
                 
-                <button style={{ display: 'none' }} onClick={() => {playAgain()}} id="playAgainButton" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Play again</button>
+                <button style={{ display: playAgainButton ? 'inline' : 'none' }} onClick={() => {playAgain()}} id="playAgainButton" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Play again</button>
             </div>
             
         </form>
