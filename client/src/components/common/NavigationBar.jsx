c9c9419c0d840ddb02a3cb987bf3451950ca77a3
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-07-14 17:49:55 -0400

    fixing navigation bar link.

--- client/src/components/common/NavigationBar.jsx
+++ client/src/components/common/NavigationBar.jsx
@@ -54,7 +54,7 @@ function NavigationBar() {
                             <NavLink to="/comosedice/menu" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/comosedice/dailychallenge" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leaderboard</NavLink>
+                            <NavLink to="/comosedice/comingsoon" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leaderboard</NavLink>
                         </li>
                         </ul>
                     </div>
