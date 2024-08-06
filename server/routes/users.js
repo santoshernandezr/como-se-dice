173979a54b4ed77570f770c1265ce0fcfa38303a
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-08-06 19:35:51 -0400

    Using bcrypt to encrypt password when adding password to database, and decrypting when comparing.

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,35 +2,16 @@ const express = require("express");
 const { connectToDb, getDb } = require("../helpers/db");
 const usersRouter = express.Router();
 
+// Requirements to hash password.
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 let db;
 connectToDb((err) => {
   if (!err) {
     db = getDb();
   }
 });
-
-/**
- * Function that will create a new user to be added to the database with their details.
- *
- * @param { JSON object } user JSON object that came in from the client.
- * @returns New user object with the users details.
- */
-function createUserObject(user) {
-  return {
-    name: user.name,
-    email: user.email,
-    timedGameMode: {
-      bestScore: 0,
-    },
-    username: user.username,
-    password: user.password,
-    dailyChallengeMode: {
-      history: [],
-      dailyChallengeCompleted: false,
-    },
-    profilePicture: user.profilePicture,
-  };
-}
 
 /**
  * Endpoint that will try and get the user from the database. If the user exists, we check the password
@@ -43,15 +24,65 @@ usersRouter.post("/signin", (req, res) => {
     .findOne({ email: req.body.email })
     .then((user) => {
       if (user != null) {
-        if (req.body.password === user.password) {
-          res.status(200).json({ msg: "Login successful", player: user });
-        } else {
-          res.status(403).json({ msg: "Password did not match" });
-        }
+        /*
+         Using the bcrypt .compare method to compare the hashed pasword that's in the database with the 
+         password (converted into hash password) the user tried using when logging in.
+        */
+        bcrypt.compare(req.body.password, user.password, (error, response) => {
+          if (error) {
+            console.log("Error: " + error);
+          }
+
+          if (response) {
+            res.status(200).json({ msg: "Login successful", player: user });
+          } else {
+            res.status(403).json({ msg: "Password did not match" });
+          }
+        });
       } else {
         res.status(404).json({ msg: "User does not exist" });
       }
     });
+});
+
+/**
+ * Endpoint that will add the new user object with their details to the database.
+ */
+usersRouter.put("/signup", async (req, res) => {
+  /*
+   Using the bcrypt .hash method to hash the users password in the database.
+   NOTE: The hashed password will be stored in the variable 'hash', which is what we'll pass in the 
+   'password' field in the object we create for the user.
+   */
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log("Error: " + err);
+    }
+
+    db.collection("Users")
+      .insertOne({
+        name: req.body.name,
+        email: req.body.email,
+        timedGameMode: {
+          bestScore: 0,
+        },
+        username: req.body.username,
+        password: hash,
+        dailyChallengeMode: {
+          history: [],
+          dailyChallengeCompleted: false,
+        },
+        profilePicture: req.body.profilePicture,
+      })
+      .then(() => {
+        res.status(200).json({ msg: "User added" });
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ msg: "Something went wrong while adding the user" });
+      });
+  });
 });
 
 /**
@@ -81,17 +112,6 @@ usersRouter.get("/emailExists/:email", (req, res) => {
       } else {
         res.json({ emailAvailable: true });
       }
-    });
-});
-
-/**
- * Endpoint that will add the new user object with their details to the database.
- */
-usersRouter.put("/signup", async (req, res) => {
-  db.collection("Users")
-    .insertOne(createUserObject(req.body))
-    .then(() => {
-      res.status(200).json({ msg: "User added" });
     });
 });
 
