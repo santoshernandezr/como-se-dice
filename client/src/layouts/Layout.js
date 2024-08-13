25ddfbfab1d88d7e000bb0958161e75b00ff2052
Roberto Santos Hernandez <robertos.0917@gmail.com>
2024-08-12 23:01:06 -0400

    initial changes to add session cookie.

--- client/src/layouts/Layout.js
+++ client/src/layouts/Layout.js
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "../css/App.css";
 import NavigationBar from "../components/common/NavigationBar";
 import Footer from "../components/common/Footer";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import UserContext from "../pages/UserContext";
+import axios from "axios";
 
 /**
  * Component responsible for the creation of the skeleton of the entire game. It consists of the Navigation bar and the
@@ -16,9 +17,40 @@ import UserContext from "../pages/UserContext";
  * @returns Layout component.
  */
 function Layout() {
-  const { user } = useContext(UserContext);
+  // Importing the 'login' method to set the user context once we verify the user has a session going on.
+  const { login } = useContext(UserContext);
+  const [loggedIn, setLoggedIn] = useState(false);
 
-  return user ? (
+  // Method that will allow us to navigate to other pages.
+  const navigate = useNavigate();
+
+  axios.defaults.withCredentials = true;
+  // UseEffect that's ONLY ran ONCE when the layout is rendered to verify the user has a valid session.
+  useEffect(() => {
+    // Call the endpoint that checks if the user has a valid session going on.
+    axios
+      .get("/signedIn")
+      .then((response) => {
+        // If the user has a valid session, then re-set the user context with 'response.data.user'.
+        if (response.data.valid) {
+          console.log("session is valid");
+          console.log("username: " + response.data.user.username);
+          login(response.data.user);
+          setLoggedIn(true);
+        }
+        // If the user DOES NOT have a valid session, then navigate to the signed in page.
+        else {
+          console.log("Session is not valid");
+          setLoggedIn(false);
+          navigate("/signin");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return loggedIn ? (
     <div>
       <NavigationBar />
 
@@ -29,7 +61,7 @@ function Layout() {
       <Footer />
     </div>
   ) : (
-    <Navigate to="/signin" />
+    <div>Loading</div>
   );
 }
 
